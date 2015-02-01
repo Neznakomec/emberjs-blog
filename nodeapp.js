35c0276c5033101ebccb091f3207f6b40132f243
Neznakomec <misha-ershov@ya.ru>
2015-02-01 13:01:54 +0300

    first initial commit

---
+++ nodeapp.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Ershov on 23.01.2015.
+ */
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+
+http.createServer(function (req, res) {
+    var info;
+
+    if (req.url == '/session') {
+        if (req.method == 'POST')
+        {
+            var body = '';
+
+            req.on('data', function (data) {
+                body += data;
+                // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                if (body.length > 1e6) {
+                    // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                    request.connection.destroy();
+                }
+            });
+
+            req.on('end', function () {
+
+                var POST = qs.parse(body);
+                // use POST
+                var POST_RES = {
+                    session: {
+                        auth_token: '1',
+                        account_id: POST["session[login_or_email]"]
+                    }
+                }
+
+                res.end(JSON.stringify(POST_RES));
+
+            });
+        }
+
+
+    } else {
+        if (req.url == '/') req.url = 'index.html';
+        else req.url = '.' + req.url;
+
+        info = fs.readFile(req.url, function(err, info){ // callback function
+            if (err){
+                console.error(err);
+                res.statusCode = 500;
+                res.end("На сервере произошла ошибка");
+                return;
+            }
+            res.end(info);
+        });
+    }
+}).listen(3000);
